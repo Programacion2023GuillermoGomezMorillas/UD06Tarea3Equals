d4ac749b3dd841950def760720f17482fd05a494
Guillermo1KDAM <guigommor@alu.edu.gva.es>
2024-02-09 19:13:45 +0100

    prueba mensaje errores

--- src/main/java/com/example/ud06herenciajavafx/controllers/AlumnoController.java
+++ src/main/java/com/example/ud06herenciajavafx/controllers/AlumnoController.java
@@ -1,16 +1,14 @@
 package com.example.ud06herenciajavafx.controllers;
 
+import com.example.ud06herenciajavafx.model.Alumno;
 import com.example.ud06herenciajavafx.model.Curso;
+import com.example.ud06herenciajavafx.model.Persona;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -50,7 +48,7 @@ public class AlumnoController implements Initializable {
 
         @FXML
         void onClickGuardar(ActionEvent event) {
-
+                Alumno alumno = crearAlumno();
         }
 
         @Override
@@ -67,5 +65,37 @@ public class AlumnoController implements Initializable {
                 }
                 //Seleccionamos el primero
                 cbCurso.setValue(cursos[0].toString());
+        }
+        private void iniciaAlertaError(String mensaje){
+                //creamos la alerta de tipo Error
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText(null);
+                alert.setTitle("Error");
+                //mostramos el mensaje
+                alert.setContentText(mensaje);
+                alert.showAndWait();
+        }
+        private Alumno crearAlumno(){
+                Alumno alumno=null;
+                String nombre=tfNombre.getText();
+                //obtenemos el dni
+                String dni = tfDni.getText();
+                //curso seleccionado
+                Curso curso = Curso.valueOf(cbCurso.getValue());
+                //edad
+                int edad;
+
+                //comprobación de errores
+                //DNI
+                if(!Persona.esCorrectoNIF(dni)){
+                        iniciaAlertaError("El DNI es incorrecto");
+                        tfDni.requestFocus();//ponemos el foco en el campo nombre
+                }
+                else if (nombre.isEmpty()){
+                        iniciaAlertaError("El alumno tiene que tener nombre");
+                        tfNombre.requestFocus();
+                        //edad
+                }
+                return alumno;
         }
 }
